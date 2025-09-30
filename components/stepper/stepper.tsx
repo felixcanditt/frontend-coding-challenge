abae743f52a1b9434f0015538cc79b4b09807a9d
Felix Canditt <felix.canditt@gmail.com>
2025-09-30 16:02:42 +0200

    rendered steps

--- components/stepper/stepper.tsx
+++ components/stepper/stepper.tsx
@@ -1,20 +1,21 @@
 import {Step} from "./steps";
 
 interface StepperProps {
-	currentStep: number,
-	steps: Step[]
+    currentStep: number,
+    steps: Step[]
 }
 
-export default function Stepper({currentStep, steps }: StepperProps) {
-	/*TODO: Replace this with the actual Stepper implementation*/
-	return (
-		<div
-			className={
-				"h-[100px] border border-dashed border-gray-200 rounded flex justify-center items-center"
-			}
-		>
-			<code className={"text-2xl"}>{"<Stepper />"}</code>
-			<div>{steps[currentStep].title}</div>
-		</div>
-	);
+export default function Stepper({currentStep, steps}: StepperProps) {
+    /*TODO: Replace this with the actual Stepper implementation*/
+    return (
+        <div
+            className={
+                ""
+            }
+        >
+            {steps.map((step, index) => (
+                <span key={index}>{step.title}</span>
+            ))}
+        </div>
+    );
 }
