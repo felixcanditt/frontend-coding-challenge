1af9965d2de19db0484160c8740e07b5cd59d44b
Felix Canditt <felix.canditt@gmail.com>
2025-09-30 18:28:56 +0200

    cleaned up styles

--- components/stepper/stepper.tsx
+++ components/stepper/stepper.tsx
@@ -7,35 +7,31 @@ interface StepperProps {
 
 export default function Stepper({currentStep, steps}: StepperProps) {
     return (
-        <ul
-            className="px-[60px] flex justify-center"
-        >
+        <ul className="flex justify-center">
             {steps.map((step, index) => (
-                <li
-                    key={index}
-                    className="flex"
-                >
+                <li key={index} className="flex">
                     {index != 0 &&
                         <div
-                            className={`mt-[13px] lg:mt-[17px] w-[30px] lg:w-[40px] h-[4px] lg:h-[6px] ${currentStep >= index ? "bg-blue-500" : "bg-gray-200"}`}>
+                            className={`-mx-[1px] mt-[13px] lg:mt-[17px] w-[52px] lg:w-[69px] h-[4px] lg:h-[6px] ${currentStep >= index ? "bg-blue-500" : "bg-gray-200"}`}
+                        >
                         </div>
                     }
                     <div className="flex flex-col items-center">
                         <div
-                            className={`w-[30px] lg:w-[40px] h-[30px] lg:h-[40px] rounded-full flex justify-center items-center ${currentStep >= index ? "bg-blue-500" : "bg-gray-200"}`}>
+                            className={`w-[30px] lg:w-[40px] h-[30px] lg:h-[40px] rounded-full flex justify-center items-center ${currentStep >= index ? "bg-blue-500" : "bg-gray-200"}`}
+                        >
                                 <span
-                                    className={`text-[12px] lg:text-[14px] font-semibold ${currentStep >= index ? "text-white" : "text-gray-700"}`}>
+                                    className={`text-[12px] lg:text-[14px] font-semibold ${currentStep >= index ? "text-white" : "text-gray-700"}`}
+                                >
                                     {index + 1}
                                 </span>
                         </div>
                         <span
                             className={`mt-[2px] lg:mt-[4px] leading-[16px] lg:leading-[24px] text-[12px] lg:text-[16px] font-semibold ${currentStep >= index ? "text-gray-700" : "text-gray-500"}`}
                         >
-                                {step.title}
-                            </span>
+                            {step.title}
+                        </span>
                     </div>
-
-
                 </li>
             ))}
         </ul>
