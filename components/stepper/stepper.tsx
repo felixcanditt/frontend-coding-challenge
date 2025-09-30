24147d2771429630db074f2f7234776590fb6a3d
Felix Canditt <felix.canditt@gmail.com>
2025-09-30 17:15:39 +0200

    styling wip

--- components/stepper/stepper.tsx
+++ components/stepper/stepper.tsx
@@ -7,20 +7,28 @@ interface StepperProps {
 
 export default function Stepper({currentStep, steps}: StepperProps) {
     return (
-        <div
-            className={
-                ""
-            }
+        <ul
+            className="flex"
         >
             {steps.map((step, index) => (
-                <div key={index}>
+                <li
+                    key={index}
+                    className="flex flex-col items-center"
+                >
+                    <div
+                        className={`w-[30px] lg:w-[40px] h-[30px] lg:h-[40px] flex justify-center items-center ${currentStep >= index ? "bg-blue-500" : "bg-gray-200"}`}>
+                        <span
+                            className={`text-[12px] lg:text-[14px] font-semibold ${currentStep >= index ? "text-white" : "text-gray-700"}`}>
+                            {index + 1}
+                        </span>
+                    </div>
                     <span
                         className={`leading-[16px] lg:leading-[24px] text-[12px] lg:text-[16px] font-semibold ${currentStep >= index ? "text-gray-700" : "text-gray-500"}`}
                     >
                         {step.title}
                     </span>
-                </div>
+                </li>
             ))}
-        </div>
+        </ul>
     );
 }
