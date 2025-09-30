53f20fc3023451c2d6df97c9171718d34e09e7b7
Felix Canditt <felix.canditt@gmail.com>
2025-09-30 17:27:01 +0200

    styling wip

--- components/stepper/stepper.tsx
+++ components/stepper/stepper.tsx
@@ -8,7 +8,7 @@ interface StepperProps {
 export default function Stepper({currentStep, steps}: StepperProps) {
     return (
         <ul
-            className="flex"
+            className="flex justify-center"
         >
             {steps.map((step, index) => (
                 <li
@@ -16,14 +16,14 @@ export default function Stepper({currentStep, steps}: StepperProps) {
                     className="flex flex-col items-center"
                 >
                     <div
-                        className={`w-[30px] lg:w-[40px] h-[30px] lg:h-[40px] flex justify-center items-center ${currentStep >= index ? "bg-blue-500" : "bg-gray-200"}`}>
+                        className={`w-[30px] lg:w-[40px] h-[30px] lg:h-[40px] rounded-full flex justify-center items-center ${currentStep >= index ? "bg-blue-500" : "bg-gray-200"}`}>
                         <span
                             className={`text-[12px] lg:text-[14px] font-semibold ${currentStep >= index ? "text-white" : "text-gray-700"}`}>
                             {index + 1}
                         </span>
                     </div>
                     <span
-                        className={`leading-[16px] lg:leading-[24px] text-[12px] lg:text-[16px] font-semibold ${currentStep >= index ? "text-gray-700" : "text-gray-500"}`}
+                        className={`mt-[2px] lg:mt-[4px] leading-[16px] lg:leading-[24px] text-[12px] lg:text-[16px] font-semibold ${currentStep >= index ? "text-gray-700" : "text-gray-500"}`}
                     >
                         {step.title}
                     </span>
