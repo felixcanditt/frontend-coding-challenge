5e076451a9a04423315be6ee8f3a983c0a133dc2
Felix Canditt <felix.canditt@gmail.com>
2025-10-01 12:46:15 +0200

    fixed text position

--- components/stepper/stepper.tsx
+++ components/stepper/stepper.tsx
@@ -20,17 +20,19 @@ export default function Stepper({currentStep, steps}: StepperProps) {
                         <div
                             className={`w-[30px] lg:w-[40px] h-[30px] lg:h-[40px] rounded-full flex justify-center items-center ${currentStep >= index ? "bg-blue-500" : "bg-gray-200"}`}
                         >
-                                <span
-                                    className={`text-[12px] lg:text-[14px] font-semibold ${currentStep >= index ? "text-white" : "text-gray-700"}`}
-                                >
-                                    {index + 1}
-                                </span>
+                            <span
+                                className={`text-[12px] lg:text-[14px] font-semibold ${currentStep >= index ? "text-white" : "text-gray-700"}`}
+                            >
+                                {index + 1}
+                            </span>
                         </div>
-                        <span
-                            className={`mt-[2px] lg:mt-[4px] leading-[16px] lg:leading-[24px] text-[12px] lg:text-[16px] font-semibold ${currentStep >= index ? "text-gray-700" : "text-gray-500"}`}
-                        >
-                            {step.title}
-                        </span>
+                        <div className="mt-[2px] lg:mt-[4px] w-[30px] lg:w-[40px] flex justify-center">
+                            <span
+                                className={`leading-[16px] lg:leading-[24px] text-[12px] lg:text-[16px] font-semibold ${currentStep >= index ? "text-gray-700" : "text-gray-500"}`}
+                            >
+                                {step.title}
+                            </span>
+                        </div>
                     </div>
                 </li>
             ))}
